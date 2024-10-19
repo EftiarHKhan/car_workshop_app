9d2a8afe0c38b4d26a7e86837bf1deae63ab95e3
EftiarHKhan <mdeftiar.official@gmail.com>
2024-10-19 23:38:16 +0600

    implement create booking flow with firebase using dummy data

--- lib/app/pages/create_booking/controllers/create_booking_controller.dart
+++ lib/app/pages/create_booking/controllers/create_booking_controller.dart
@@ -1,11 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-  import '/app/core/base/base_controller.dart';
-  
+import '/app/core/base/base_controller.dart';
+
 class CreateBookingController extends BaseController {
- @override
+  final carMakeController = TextEditingController();
+  final carModelController = TextEditingController();
+  final carYearController = TextEditingController();
+  final carRegistrationPlateController = TextEditingController();
+
+  final customerNameController = TextEditingController();
+  final customerEmailController = TextEditingController();
+  final customerPhoneController = TextEditingController();
+
+  final bookingTitleController = TextEditingController();
+  final startDate = ''.obs;
+  final endDate = ''.obs;
+  final mechanicList = Rx<List<String>?>(null);
+  final selectedMechanic = ''.obs;
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
   Future<void> onInit() async {
     super.onInit();
+    mechanicList.value = ['John Doe', 'Jane Doe', 'John Smith'];
   }
 
+  Future<void> onTapSubmit() async {
+    // Submit booking logic here
+
+    dummyFunction();
+
+    // save booking to Firestore
+    await _firestore.collection('bookings').add(
+      {
+        'carMake': carMakeController.text.trim(),
+        'carModel': carModelController.text.trim(),
+        'carYear': carYearController.text.trim(),
+        'carRegistrationPlate': carRegistrationPlateController.text.trim(),
+        'customerName': customerNameController.text.trim(),
+        'customerEmail': customerEmailController.text.trim(),
+        'customerPhone': customerPhoneController.text.trim(),
+        'bookingTitle': bookingTitleController.text.trim(),
+        'startDate': startDate.value,
+        'endDate': endDate.value,
+        'mechanic': selectedMechanic.value,
+      },
+    );
+  }
+
+  void dummyFunction() {
+    // Dummy function
+    carMakeController.text = 'Toyota';
+    carModelController.text = 'Corolla';
+    carYearController.text = '2020';
+    carRegistrationPlateController.text = 'ABC1234';
+    customerNameController.text = 'John Doe';
+    customerEmailController.text = 'johndoe@gmail.com';
+    customerPhoneController.text = '1234567890';
+    bookingTitleController.text = 'Oil Change';
+    startDate.value = '2024-12-01';
+    endDate.value = '2024-12-02';
+    selectedMechanic.value = 'hridoy@gmail.com';
+  }
 }
-  
--- lib/app/pages/create_booking/views/create_booking_view.dart
+++ lib/app/pages/create_booking/views/create_booking_view.dart
@@ -1,27 +1,157 @@
+import 'package:car_workshop_app/app/core/base/base_controller.dart';
+import 'package:dropdown_flutter/custom_dropdown.dart';
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
 import '/app/core/base/base_view.dart';
 import '/app/pages/create_booking/controllers/create_booking_controller.dart';
 
 //ignore: must_be_immutable
 class CreateBookingView extends BaseView<CreateBookingController> {
   CreateBookingView({super.key});
-    
+
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return AppBar(
       title: Text('Create Booking'),
     );
   }
-  
+
   @override
   Widget body(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text('CreateBookingView'),
-        ],
+      padding: const EdgeInsets.all(16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('CreateBookingView'),
+            16.height,
+            _buildCarDetails(),
+            16.height,
+            _buildCustomerDetails(),
+            16.height,
+            _buildBookingDetails(),
+            16.height,
+            _buildAssignMechanic(),
+            16.height,
+            _buildSubmitButton(),
+            16.height,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCarDetails() {
+    return Column(
+      children: [
+        Text('Car Details'),
+        16.height,
+        TextFormField(
+          controller: controller.carMakeController,
+          decoration: const InputDecoration(
+            labelText: 'Car Make',
+          ),
+        ),
+        TextFormField(
+          controller: controller.carModelController,
+          decoration: const InputDecoration(
+            labelText: 'Car Model',
+          ),
+        ),
+        TextFormField(
+          controller: controller.carYearController,
+          decoration: const InputDecoration(
+            labelText: 'Car Year',
+          ),
+        ),
+        TextFormField(
+          controller: controller.carRegistrationPlateController,
+          decoration: const InputDecoration(
+            labelText: 'Car Registration Plate',
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCustomerDetails() {
+    return Column(
+      children: [
+        Text('Customer Details'),
+        16.height,
+        TextFormField(
+          controller: controller.customerNameController,
+          decoration: const InputDecoration(
+            labelText: 'Customer Name',
+          ),
+        ),
+        TextFormField(
+          controller: controller.customerEmailController,
+          decoration: const InputDecoration(
+            labelText: 'Customer Email',
+          ),
+        ),
+        TextFormField(
+          controller: controller.customerPhoneController,
+          decoration: const InputDecoration(
+            labelText: 'Customer Phone',
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBookingDetails() {
+    return Column(
+      children: [
+        Text('Booking Details'),
+        16.height,
+        TextFormField(
+          controller: controller.bookingTitleController,
+          decoration: const InputDecoration(
+            labelText: 'Booking Title',
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAssignMechanic() {
+    return Column(
+      children: [
+        Text('Assign Mechanic'),
+        16.height,
+        DropdownFlutter<String>(
+          hintText: 'Select Mechanic',
+          items: controller.mechanicList.value,
+          initialItem: controller.mechanicList.value![0],
+          onChanged: (value) {
+            controller.selectedMechanic.value = value!;
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSubmitButton(){
+    return InkWell(
+      onTap: controller.onTapSubmit,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 32,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Text(
+          'Submit',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
 }
-  
